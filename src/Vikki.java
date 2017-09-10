c72bfecaa1932b50084bedeb6823932b331c1636
balajisundaramm <balajitpgit13@gmail.com>
2017-09-10 20:11:06 +0530

    Saving programs done so far

---
+++ src/Vikki.java
@@ -0,0 +1,19 @@
+public class Vikki
+{
+	public static void main(String[] args)
+	{
+		int a=55;
+		int b=3;
+		int c= reminder (a,b);
+		
+		System.out.println("Reminder = "+c);
+		
+	}
+	public static int reminder(int a, int b)
+	{
+		String
+		int c= 34>>>2;
+		int d= a/b;
+		return c;
+	}
+}
